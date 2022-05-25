6e78f8562f9834475ab049eabde9019057167688
Luke Volpatti <luke.volpatti@gmail.com>
2022-05-25 12:54:37 -0700

    Added license and pyproject, updated setup.py. Project ready for release on PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="qss",
-    version="0.1",
+    version="0.1.1",
+    author="Luke Volpatti",
+    description="QSS: the quadratic-separable solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy", "scipy", "qdldl", "cvxpy"],
+    url="https://github.com/lukevolpatti/qss",
+    project_urls={
+        "Bug Tracker": "https://github.com/lukevolpatti/qss/issues",
+    },
+    license="Apache 2.0",
     packages=["qss"],
+    python_requires=">=3.6",
 )
