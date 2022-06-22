f6be3b345238ef38adbf649f66a94a44f48cc195
Luke Volpatti <luke.volpatti@gmail.com>
2022-06-22 15:54:05 -0700

    Added adam to proj_sd

--- qss/polish.py
+++ qss/polish.py
@@ -152,7 +152,15 @@ def proj_sd(
     prev_t = 1
 
     if method == "momentum":
+        momentum = 0.9
         prev_step = np.zeros(dim)
+    elif method == "adam":
+        beta1 = 0.9
+        beta2 = 0.999
+        beta1t = beta1
+        beta2t = beta2
+        m_adam = 0
+        v_adam = 0
 
     new_kkt = sp.sparse.vstack(
         [
@@ -176,57 +184,75 @@ def proj_sd(
         v_st = F.solve(np.concatenate([v_st_np, np.zeros_like(b_constr)]))[:dim]
 
         if method == "momentum":
-            v_st = 0.99 * prev_step + v_st
+            v_st = momentum * prev_step + v_st
             prev_step = v_st
+        elif method == "adam":
+            v_st = -v_st
+            m_adam = beta1 * m_adam + (1 - beta1) * v_st
+            v_adam = beta2 * v_adam + (1 - beta2) * v_st * v_st
+            mhat = m_adam / (1 - beta1t)
+            vhat = v_adam / (1 - beta2t)
+            beta1t *= beta1
+            beta2t *= beta2
+            x = x - 0.001 * mhat / (np.sqrt(vhat) + 1e-8)
 
-        left_t = 0.5 * prev_t
-        mid_t = prev_t
-        right_t = 2 * prev_t
+        if method == "momentum":
+            left_t = 0.5 * prev_t
+            mid_t = prev_t
+            right_t = 2 * prev_t
 
-        a = 0.5 * x @ P @ x + q @ x + r
-        b = x @ P @ v_st + q @ v_st
-        c = 0.5 * v_st @ P @ v_st
+            a = 0.5 * x @ P @ x + q @ x + r
+            b = x @ P @ v_st + q @ v_st
+            c = 0.5 * v_st @ P @ v_st
 
-        left_t_obj = sd_eval_obj(x, v_st, a, b, c, left_t, g, equil_scaling, obj_scale)
-        mid_t_obj = sd_eval_obj(x, v_st, a, b, c, mid_t, g, equil_scaling, obj_scale)
-        right_t_obj = sd_eval_obj(
-            x, v_st, a, b, c, right_t, g, equil_scaling, obj_scale
-        )
+            left_t_obj = sd_eval_obj(
+                x, v_st, a, b, c, left_t, g, equil_scaling, obj_scale
+            )
+            mid_t_obj = sd_eval_obj(
+                x, v_st, a, b, c, mid_t, g, equil_scaling, obj_scale
+            )
+            right_t_obj = sd_eval_obj(
+                x, v_st, a, b, c, right_t, g, equil_scaling, obj_scale
+            )
 
-        found_t = False
-        while not found_t:
-            if mid_t_obj <= left_t_obj and mid_t_obj <= right_t_obj:
-                found_t = True
-            else:
-                if mid_t_obj > left_t_obj:
-                    right_t = mid_t
-                    right_t_obj = mid_t_obj
-                    mid_t = left_t
-                    mid_t_obj = left_t_obj
-                    left_t = 0.5 * left_t
-                    left_t_obj = sd_eval_obj(
-                        x, v_st, a, b, c, left_t, g, equil_scaling, obj_scale
-                    )
-                elif mid_t_obj > right_t_obj:
-                    left_t = mid_t
-                    left_t_obj = mid_t_obj
-                    mid_t = right_t
-                    mid_t_obj = right_t_obj
-                    right_t *= 2
-                    right_t_obj = sd_eval_obj(
-                        x, v_st, a, b, c, right_t, g, equil_scaling, obj_scale
-                    )
+            found_t = False
+            while not found_t:
+                if mid_t_obj <= left_t_obj and mid_t_obj <= right_t_obj:
+                    found_t = True
+                else:
+                    if mid_t_obj > left_t_obj:
+                        right_t = mid_t
+                        right_t_obj = mid_t_obj
+                        mid_t = left_t
+                        mid_t_obj = left_t_obj
+                        left_t = 0.5 * left_t
+                        left_t_obj = sd_eval_obj(
+                            x, v_st, a, b, c, left_t, g, equil_scaling, obj_scale
+                        )
+                    elif mid_t_obj > right_t_obj:
+                        left_t = mid_t
+                        left_t_obj = mid_t_obj
+                        mid_t = right_t
+                        mid_t_obj = right_t_obj
+                        right_t *= 2
+                        right_t_obj = sd_eval_obj(
+                            x, v_st, a, b, c, right_t, g, equil_scaling, obj_scale
+                        )
+
+            if iter % 50 == 0:
+                print("t:", mid_t)
+
+            x = x + mid_t * v_st
+            prev_t = mid_t
 
         if iter % 50 == 0:
-            print("t:", mid_t)
-            print("obj:", mid_t_obj)
+            print(
+                "obj:", util.evaluate_objective(P, q, r, g, x, obj_scale, equil_scaling)
+            )
 
-        x = x + mid_t * v_st
-        prev_t = mid_t
-
-        if prev_mid_t_obj - mid_t_obj < 1e-7:  # TODO: better stopping crit
-            converged = True
-        else:
-            prev_mid_t_obj = mid_t_obj
+        # if prev_mid_t_obj - mid_t_obj < 1e-7:  # TODO: better stopping crit
+        #     converged = True
+        # else:
+        #     prev_mid_t_obj = mid_t_obj
 
     return x, iter
